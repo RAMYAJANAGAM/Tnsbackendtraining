2c88d72c02a7a8982b0c3cf86ba97fbc55707c21
JANAGAM RAMYA <janagamramya000@gmail.com>
2025-11-05 13:39:57 +0530

    first commit

--- Tnsbackendtraining/src/mypack/BikeMain.java
+++ Tnsbackendtraining/src/mypack/BikeMain.java
@@ -3,6 +3,7 @@ package mypack;
 public class BikeMain {
 	public static void main(String[] args) {
         Bike myBike = new Bike();
+        //bike details
         myBike.brand = "Yamaha";
         myBike.model = "R15";
         myBike.mileage = 45;
